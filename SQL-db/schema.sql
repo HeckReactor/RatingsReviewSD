fea13f49687021827c1f88ddcf8e813f79dc97ac
Calvin Chau <cachau33@gmail.com>
2021-08-17 12:53:28 -0700

    Edited and changed Schema, fixed syntax errors.

--- SQL-db/schema.sql
+++ SQL-db/schema.sql
@@ -1,62 +1,66 @@
-DROP DATABASE IF EXISTS ReviewsRatings;
-CREATE DATABASE ReviewsRatings;
+DROP DATABASE IF EXISTS reviewsratings;
+CREATE DATABASE reviewsratings;
 
-\c ReviewsRatings;
+\c reviewsratings;
 
-CREATE TABLE reviews (
+CREATE TABLE IF NOT EXISTS reviews (
 	review_id serial PRIMARY KEY NOT NULL,
 	product_id integer NOT NULL,
-	rating integer(1) NOT NULL,
+	rating integer NOT NULL,
 	date VARCHAR(20) NOT NULL,
 	summary VARCHAR(255) NOT NULL,
-	body VARCHAR(255) NOT NULL,
+	body TEXT NOT NULL,
 	recommend BOOLEAN NOT NULL DEFAULT 'true',
 	reported BOOLEAN NOT NULL DEFAULT 'false',
 	reviewer_name varchar(32) NOT NULL,
-	email varchar(32) NOT NULL,
+	email varchar(64) NOT NULL,
 	response VARCHAR(255) NOT NULL DEFAULT '',
 	helpfulness integer NOT NULL DEFAULT 0
 ) ;
 
-COPY reviews (review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, email, response, helpfulness )
+COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, email, response, helpfulness )
 FROM '/Users/mango/Documents/shortenedData/reviews.csv'
 DELIMITER ','
-CSV HEADER;
+CSV
+HEADER;
 
 
-CREATE TABLE photos (
+CREATE TABLE IF NOT EXISTS photos (
 	photo_id serial PRIMARY KEY NOT NULL,
 	review_id integer NOT NULL REFERENCES reviews(review_id),
 	photo_url TEXT NOT NULL
 );
 
-COPY photos (photo_id, review_id, photo_url)
+COPY photos(photo_id, review_id, photo_url)
 FROM '/Users/mango/Documents/shortenedData/reviews_photos.csv'
 DELIMITER ','
-CSV HEADER;
+CSV
+HEADER;
 
-CREATE TABLE characteristic_reviews (
+CREATE TABLE IF NOT EXISTS characteristic_reviews (
 	id serial PRIMARY KEY NOT NULL,
-	review_id integer NOT NULL REFERENCES reviews(review_id),
 	characteristic_id integer NOT NULL,
+	review_id integer NOT NULL REFERENCES reviews(review_id),
 	value integer NOT NULL
 );
 
-COPY characteristics_reviews (id, review_id, characteristic_id)
+COPY characteristic_reviews(id, characteristic_id, review_id, value)
 FROM '/Users/mango/Documents/shortenedData/characteristic_reviews.csv'
 DELIMITER ','
-CSV HEADER;
+CSV
+HEADER;
 
-CREATE TABLE characteristics (
+CREATE TABLE IF NOT EXISTS characteristics (
 	characteristic_id serial PRIMARY KEY NOT NULL,
 	product_id integer NOT NULL,
 	name varchar(16) NOT NULL
 );
 
-COPY characteristics (characteristic_id, product_id, name)
+COPY characteristics(characteristic_id, product_id, name)
 FROM '/Users/mango/Documents/shortenedData/characteristics.csv'
 DELIMITER ','
-CSV HEADER;
+CSV
+HEADER;
 
 
 
