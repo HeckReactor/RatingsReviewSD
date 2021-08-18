9e7f71ccfdfdd8a58c34aaaabb3021363c1cb2df
Calvin Chau <cachau33@gmail.com>
2021-08-17 22:17:55 -0700

    Fleshed out functionality of meta controller and model.

--- server/controllers/meta.js
+++ server/controllers/meta.js
@@ -1,17 +1,49 @@
+/* eslint-disable no-console */
 /* eslint-disable camelcase */
 const models = require('../models');
+const tool = require('../helperFunctions.js');
 
 module.exports = {
   get: (req, res) => {
     const { product_id } = req.query;
-    models.meta.getMeta({ product: product_id }, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(404);
-      } else {
-        res.send(data);
-      }
-    });
-  },
+    const metaData = {
+      product: product_id,
+      ratings: {},
+      recommend: {},
+      characteristics: {},
+    };
 
+    const ratings = models.meta.getRatings(req.query)
+      .then(({ rows }) => {
+        tool.ratingsObj(rows, metaData);
+      })
+      .catch((err) => {
+        console.log('Error while retrieving ratings: ', err);
+      });
+
+    const recomends = models.meta.getRecommend(req.query)
+      .then(({ rows }) => {
+        tool.recommendedObj(rows, metaData);
+      })
+      .catch((err) => {
+        console.log('Error while retrieving recommend: ', err);
+      });
+
+    const characteristics = models.meta.getCharacteristics(req.query)
+      .then(({ rows }) => {
+        tool.characteristicsList(rows, metaData);
+      })
+      .catch((err) => {
+        console.log('Error while retrieving characteristics: ', err);
+      });
+
+    Promise.all([ratings, recomends, characteristics])
+      .then(() => {
+        res.send(metaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(404);
+      });
+  },
 };
---
+++ server/models/meta.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+const db = require('../../SQL-db');
+
+module.exports = {
+  getRatings: ({ product_id }) => {
+    const query = {
+      text:
+      `SELECT json_build_object
+      (rating, count(rating)) as ratingCount
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY rating
+      ORDER BY rating;`,
+      values: [product_id],
+    };
+    return db.query(query);
+  },
+  getRecommend: ({ product_id }) => {
+    const query = {
+      text:
+      `SELECT json_build_object
+      (recommend, count(recommend)) as recommend
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY recommend;`,
+      values: [product_id],
+    };
+    return db.query(query);
+  },
+  getCharacteristics: ({ product_id }) => {
+    const query = {
+      text:
+      `SELECT json_build_object(
+        name, json_build_object(
+          'id', characteristic_id,
+          'value', avg(value)
+          )
+        ) as chars
+      FROM characteristics
+      INNER JOIN characteristic_reviews
+      ON characteristics.id = characteristic_id
+      WHERE product_id = ${product_id}
+      GROUP BY name, characteristic_id;`,
+    };
+    return db.query(query);
+  },
+};
