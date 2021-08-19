141eb38b60b681268b1c2eafe64963658ff14a42
Calvin Chau <cachau33@gmail.com>
2021-08-19 12:11:33 -0700

    Changed the names of indexed keys

--- SQL-db/schema.sql
+++ SQL-db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS reviews (
 ) ;
 
 COPY reviews(id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, email, response, helpfulness )
-FROM '/Users/mango/Documents/shortenedData/reviews.csv'
+FROM '/Users/mango/Documents/sdcData/reviews.csv'
 DELIMITER ','
 CSV
 HEADER;
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS photos (
 );
 
 COPY photos(id, review_id, url)
-FROM '/Users/mango/Documents/shortenedData/reviews_photos.csv'
+FROM '/Users/mango/Documents/sdcData/reviews_photos.csv'
 DELIMITER ','
 CSV
 HEADER;
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS characteristic_reviews (
 );
 
 COPY characteristic_reviews(id, characteristic_id, review_id, value)
-FROM '/Users/mango/Documents/shortenedData/characteristic_reviews.csv'
+FROM '/Users/mango/Documents/sdcData/characteristic_reviews.csv'
 DELIMITER ','
 CSV
 HEADER;
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS characteristics (
 );
 
 COPY characteristics(id, product_id, name)
-FROM '/Users/mango/Documents/shortenedData/characteristics.csv'
+FROM '/Users/mango/Documents/sdcData/characteristics.csv'
 DELIMITER ','
 CSV
 HEADER;
@@ -67,10 +67,10 @@ UPDATE reviews SET response = '' where response =  'null';
 -- necessary or else i won't get back any data
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO calvin;
 -- Made indexes, should be faster
-CREATE INDEX productID_index ON reviews (product_id);
-CREATE INDEX reviewID_photo_index ON photos (review_id);
-CREATE INDEX characteristicID_review_index ON characteristic_reviews (characteristic_id);
-CREATE INDEX reviewID_characteristics_index ON characteristic_reviews(review_id);
+CREATE INDEX reviews_product_id_index ON reviews (product_id);
+CREATE INDEX photo_review_id_index ON photos (review_id);
+CREATE INDEX characteristic_reviews_characteristic_id_index ON characteristic_reviews (characteristic_id);
+CREATE INDEX characteristic_reviews_review_id_index ON characteristic_reviews(review_id);
 
 
 
