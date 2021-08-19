b56029e5e2321d0e3e25739ae8f74fb2b73e7bd0
Calvin Chau <cachau33@gmail.com>
2021-08-19 16:12:50 -0700

    Changed back to use config.pool as input. Makes its easier to setup for deployment

--- SQL-db/index.js
+++ SQL-db/index.js
@@ -1,12 +1,5 @@
 const { Pool } = require('pg');
+const config = require('../.config.js');
 
-const pool = new Pool({
-  user: 'calvin',
-  host: 'localhost',
-  database: 'reviewsratings',
-  password: '',
-  port: 5432,
-  max: 15,
-  connectionTimeoutMillis: 2000,
-});
+const pool = new Pool(config.pool);
 module.exports = pool;
