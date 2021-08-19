8126a373941ca9dff91535105931c6a5a48cd252
Calvin Chau <cachau33@gmail.com>
2021-08-19 12:11:59 -0700

    Added small comment

--- SQL-db/index.js
+++ SQL-db/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 const { Pool } = require('pg');
 const config = require('../.config.js');
-
+// you opened a pool but never closed it yo
 const pool = new Pool(config.pool);
 module.exports = pool;
