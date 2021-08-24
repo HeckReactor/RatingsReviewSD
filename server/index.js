16a632cce25903708e6d46207816dc8120c654b5
Calvin Chau <cachau33@gmail.com>
2021-08-24 12:20:50 -0700

    Installed new relic to repo, to make things easier for when i make more servers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-// const path = require('path');
+require('newrelic');
 const express = require('express');
 const router = require('./routes.js');
 
@@ -11,6 +11,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.get(`/loaderio-db2bdbaa121b2f43058bc4e77f62d2b8.txt`, (req, res) => {
+//   res.status(200).send('loaderio-db2bdbaa121b2f43058bc4e77f62d2b8');
+// });
 app.use('/', router);
 
 app.listen(PORT, () => {
